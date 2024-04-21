c32a3524f817ea19f75b833f30086f59b72121ae
Adriani Delfino <141968057+Adri210@users.noreply.github.com>
2024-04-20 21:45:43 -0300

    feat: Create filter card

---
+++ src/components/FilterCard.vue
@@ -0,0 +1,88 @@
+<script setup>
+  import { ref, defineProps, defineEmits } from 'vue';
+
+  const props = defineProps({
+    selectedFilter: { type: String, required: true },
+  });
+
+  const emit = defineEmits(['select-filter']);
+
+  const filterOptions = ref(['Nome', 'Id', 'Tipo', 'Espécie']);
+</script>
+
+<template>
+  <div class="col-12 pe-4 ps-0">
+    <div class="card ps-0 mb-4 rounded-4">
+      <div class="w-100 h-100 p-4 pt-0 d-flex flex-column align-items-center">
+        <div class="d-flex align-items-center justify-content-center ps-5">
+          <h2 class="m-0">Filtre seu Pokémon</h2>
+          <img src="@/assets/images/pokebola.gif" alt="Pokebola" class="pokebola">
+        </div>
+        <div class="input-group w-50">
+          <button
+            class="btn btn-outline-secondary dropdown-toggle fw-medium"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {{ selectedFilter }}
+          </button>
+
+          <ul class="dropdown-menu">
+            <li
+              v-for="filter in filterOptions"
+              :key="filter"
+              @click="emit('select-filter', filter)"
+            >
+              <a class="dropdown-item" href="#">
+                {{ filter }}
+              </a>
+            </li>
+          </ul>
+
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Text input with dropdown button"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .card {
+    background-color: $pink-400;
+    border-color: $pink-400;
+  }
+
+  .pokebola {
+    width: 9rem;
+  }
+
+  .btn {
+    background-color: $green-400;
+    color: white;
+
+    &:hover, &:focus {
+      background-color: $blue-400;
+    }
+  }
+
+  .dropdown-item {
+    &:hover {
+      background-color: $pink-100;
+    }
+  }
+
+  @media(max-width: 720px) {
+    .input-group {
+      width: 100% !important;
+    }
+
+    h2 {
+      font-size: 1.2rem;
+    }
+  }
+</style>
--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -3,13 +3,15 @@
   import { useInfiniteScroll } from '@vueuse/core'
   import { getPokemons } from '@/axios';
   import HeaderComponent from '@/components/HeaderComponent.vue';
-  import PokemonCard from '../components/PokemonCard.vue';
-import AsideComponent from '../components/AsideComponent.vue';
+  import PokemonCard from '@/components/PokemonCard.vue';
+  import AsideComponent from '@/components/AsideComponent.vue';
+  import FilterCard from '@/components/FilterCard.vue';
   
   const pokemonsToShow = 20;
   const pokemonListEl = ref(null);
   const pokemons = ref([]);
   const loading = ref(false);
+  const selectedFilter = ref('Nome');
 
   onMounted(async () => {
     pokemons.value = await getPokemons(0, pokemonsToShow);
@@ -37,6 +39,10 @@ import AsideComponent from '../components/AsideComponent.vue';
     },
     { distance: 10 }
   )
+
+  const toggleFilter = (filter) => {
+    selectedFilter.value = filter;
+  }
 </script>
 
 <template>
@@ -44,26 +50,30 @@ import AsideComponent from '../components/AsideComponent.vue';
   
   <main class="d-flex">
     <AsideComponent />
-    <section
-      ref="pokemonListEl"
-      class="cards-container m-auto w-75 h-100 bg-white row p-4 rounded-top-4"
-    >
-    <div class="col-12">
-      <input class="w-100"/>
+
+    <div class="row m-auto w-75 h-100 bg-white rounded-top-4 py-4 ps-4">
+      <FilterCard
+        :selectedFilter="selectedFilter"
+        @select-filter="toggleFilter"
+      />
+
+      <section
+        ref="pokemonListEl"
+        class="cards-container row col-12 h-100 p-0"
+      >
+        <PokemonCard
+          v-for="(pokemon, index) in pokemons"
+          :key="`${pokemon.name}-${index}`"
+          :url="pokemon.url" 
+        />
+  
+        <img
+          class="loading m-auto d-block"
+          src="@/assets/images/pikachu-loading.gif"
+          v-show="loading"
+        />
+      </section>
     </div>
-
-      <PokemonCard
-        v-for="(pokemon, index) in pokemons"
-        :key="`${pokemon.name}-${index}`"
-        :url="pokemon.url" 
-      />
-
-      <img
-        class="loading m-auto d-block"
-        src="@/assets/images/pikachu-loading.gif"
-        v-show="loading"
-      />
-    </section>
     
     <AsideComponent />
   </main>
